5cdc25d7bd2ff52dad1080d25e4a03ea4f6536b0
theo-seamon <t_seamon@coloradocollege.edu>
2025-10-28 11:56:32 -0600

    Replaced out of date KRDU.py

--- class-projects/KRDU.py
+++ class-projects/KRDU.py
@@ -6,6 +6,7 @@ import matplotlib.dates as mdates
 import numpy as np
 
 # Loading data
+# This data represents KRDU temperatures from 1887 to 2025 (in deg C?)
 df_data = pd.read_csv('/Users/theo/Documents/CC/EV228/ev_228/data/KRDU_temp_188708-202508.csv')
 df_nov = df_data['NOV']
 df_nov = df_nov[:-1]
@@ -17,7 +18,7 @@ df_date = df_date[:-1]
 fig, ax = plt.subplots()
 ax.plot(df_date, df_nov, color='red', label='Temperature in November')
 plt.xlabel('time')
-plt.ylabel('temperature, deg F')
+plt.ylabel('temperature, deg C')
 plt.legend()
 plt.title('Average Temperature in November')
 plt.show()
